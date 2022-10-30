9c41d2a06f92104851a7240063f24037b2aa7f30
takaya-47 <local.s.s.47@gmail.com>
2022-10-31 08:14:34 +0900

    テーブル構成変更

---
+++ database/migrations/2022_10_27_082903_create_faces_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faces', function (Blueprint $table) {
+            $table->id();
+            $table->string('person_id')->nullable(); // face api利用時に必要となるもの
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faces');
+    }
+};
---
+++ database/migrations/2022_10_27_092020_add_column_face_id_to_users_and_drop_column_face_registration_flg_from_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('face_id')->nullable()->after('group_id')->constrained();
+            $table->dropColumn('face_registration_flg');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('face_id');
+            $table->boolean('face_registration_flg')->default(0)->after('group_id');
+        });
+    }
+};
